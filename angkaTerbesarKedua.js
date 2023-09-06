fab3c51427c7bc71762e4384bea930808b60058a
Ahmad Barizi <bariziahmad80@gmail.com>
2023-09-06 23:26:42 +0700

    number-3

--- angkaTerbesarKedua.js
+++ angkaTerbesarKedua.js
@@ -1,14 +1,9 @@
 var numbers = [9, 15, 10, 19, 11, 2];
-// numbers.sort((i, j) => { return 1 - Math.random() });
-// console.log(numbers.sort((i, j) => { return j-i
-//     ; }));
 
-// for (var i = 0; i < numbers.length; i++) {
-//     console.log(numbers[i]);
-//     const maxValue = numbers[i];
-// }
-
-// function arrMax(n) {
-//     return Math.max(50, ...n);
-// }
-// console.log(arrMax(numbers));
+function arrMax(n) {
+    const results = n.filter((item) => {
+       return item !== Math.max(...n)
+    });
+    return Math.max(...results);
+}
+console.log(arrMax(numbers));
